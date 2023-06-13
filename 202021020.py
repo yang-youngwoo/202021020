58d824a86f285a285768badfa508f37a3b7deef0
yang-youngwoo <136176666+yang-youngwoo@users.noreply.github.com>
2023-06-13 13:52:42 +0900

    Update 202021020.py

--- 202021020.py
+++ 202021020.py
@@ -47,6 +47,7 @@ st.pyplot(fig1)
     '''
     st.code(code, language="python")
 
+#그래프 
 fig1, ax1 = plt.subplots()
 t, y, _ = signal.lsim(s1, np.ones_like(t), t)
 ax1.plot(t, y)
@@ -54,7 +55,7 @@ ax1.set_xlabel('Time')
 ax1.set_ylabel('Output')
 st.pyplot(fig1)
 
-# 주파수 응답 보드선도 
+# 주파수 응답 보드선도 그래프
 st.header("주파수 응답 보드선도")
 
 if st.button("코드 보기2"):
@@ -82,6 +83,7 @@ st.pyplot(fig2)
     '''
     st.code(code2, language="python")
 
+#그래프 그리기
 fig2, (ax2_mag, ax2_phase) = plt.subplots(2, 1)
 ax2_mag.semilogx(w, mag)
 ax2_mag.set_ylabel('Magnitude [dB]')
